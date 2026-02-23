41046a7ebde98eb1a23f1375ed0bdec39c5db687
Sarah Batten <scb0122@auburn.edu>
2026-02-23 15:42:08 -0600

    finished first two functions, get_reverse and get_complement

--- translate.py
+++ translate.py
@@ -3,31 +3,7 @@
 import sys
 
 def translate_sequence(rna_sequence, genetic_code):
-    """Translates a sequence of RNA into a sequence of amino acids.
 
-    Translates `rna_sequence` into string of amino acids, according to the
-    `genetic_code` given as a dict. Translation begins at the first position of
-    the `rna_sequence` and continues until the first stop codon is encountered
-    or the end of `rna_sequence` is reached.
-
-    If `rna_sequence` is less than 3 bases long, or starts with a stop codon,
-    an empty string is returned.
-
-    Parameters
-    ----------
-    rna_sequence : str
-        A string representing an RNA sequence (upper or lower-case).
-
-    genetic_code : dict
-        A dictionary mapping all 64 codons (strings of three RNA bases) to
-        amino acids (string of single-letter amino acid abbreviation). Stop
-        codons should be represented with asterisks ('*').
-
-    Returns
-    -------
-    str
-        A string of the translated amino acids.
-    """
     pass
 
 def get_all_translations(rna_sequence, genetic_code):
@@ -64,31 +40,14 @@ def get_all_translations(rna_sequence, genetic_code):
     pass
 
 def get_reverse(sequence):
-    """Reverse orientation of `sequence`.
-
-    Returns a string with `sequence` in the reverse order.
-
-    If `sequence` is empty, an empty string is returned.
-
-    Examples
-    --------
-    >>> get_reverse('AUGC')
-    'CGUA'
-    """
+    sequence = sequence.upper()
+    return sequence[::-1]
     pass
 
 def get_complement(sequence):
-    """Get the complement of a `sequence` of nucleotides.
-
-    Returns a string with the complementary sequence of `sequence`.
-
-    If `sequence` is empty, an empty string is returned.
-
-    Examples
-    --------
-    >>> get_complement('AUGC')
-    'UACG'
-    """
+    sequence = sequence.upper()
+    complementary = str.maketrans('AUCGaucg', 'UAGCuagc')
+    return sequence.translate(complementary)
     pass
 
 def reverse_and_complement(sequence):
