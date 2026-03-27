69f94be46f2df3f77de611137dd7cdbbd4a5d37c
Sarah Batten <scb0122@auburn.edu>
2026-03-27 13:42:33 -0500

    Adding docstring

--- translate_orf.py
+++ translate_orf.py
@@ -6,6 +6,44 @@ import translate
 import find_orf
 
 
+# copying the translate_first_orf function
+
+def translate_first_orf(sequence,
+    start_codons = ['AUG'],
+    stop_codons = ['UAA', 'UAG', 'UGA'],
+    genetic_code = {
+        'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T',
+        'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I',
+        'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H',
+        'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P',
+        'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C',
+        'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S',
+        'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*',
+        'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I',
+        'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L',
+        'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A',
+        'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R',
+        'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I',
+        'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'
+        },
+    ):
+
+"""
+The goal of this script is to translate the first open reading frame in a DNA or RNA sequence. It works by recognizing the start codon, reading the nucleotides by threes after this point, and stopping when a stop codon is reached. 
+
+Parameters
+-----------
+sequence: a string, given to the script, that contains a DNA or RNA sequence
+start_codons: a list of strings, contains all possible start codons. 
+
+stop_codons: a list of strings, contains all possible stop codons.
+
+genetic_code: a dictionary, contains the genetic code by mapping each possible three letter nucleotide pair to its corresponding one letter amino acid. 
+Outputs
+-------
+str: a string of the amino acid sequence that is encoded by the first open reading frame encountered along the DNA/RNA sequence. It begins with the start_codons and ends with one of the stop_codons. If no open reading frame is found, an empty string is returned. 
+"""
+
 # copying main function from find_orf.py as a basis for writing this main function
 
 def main():
